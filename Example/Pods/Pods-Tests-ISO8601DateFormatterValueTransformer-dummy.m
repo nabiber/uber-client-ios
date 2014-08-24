63fc4786228bfa75c59a0d87457473a6ef261eb1
Niaz <niaz.jalal@gmail.com>
2014-08-24 15:00:45 -0700

    Adding RestKit dependency to POD

---
+++ Example/Pods/Pods-Tests-ISO8601DateFormatterValueTransformer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tests_ISO8601DateFormatterValueTransformer : NSObject
+@end
+@implementation PodsDummy_Pods_Tests_ISO8601DateFormatterValueTransformer
+@end
